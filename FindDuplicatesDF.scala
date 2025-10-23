81c979f03bcc51b3626bd4767636be61a1bc3b2e
zakeerm855 <zakeerm855@gmail.com>
2025-10-23 16:36:01 +0530

    scala file added

---
+++ FindDuplicatesDF.scala
@@ -0,0 +1,17 @@
+package SparkExample.Interview
+
+import org.apache.spark.sql.SparkSession
+
+object FindDuplicatesDF {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().master("local").appName("find duplicates in DF").getOrCreate()
+    val data1 = Seq(10, 20, 20, 30, 30, 30, 40)
+
+    import spark.implicits._
+
+    val df1 = data1.toDF("value")
+    val dupes = df1.groupBy("value").count.filter("count > 1")
+    dupes.show()
+  }
+}
